0c46c85ba75b60bb6875fe552cf88defbaa86160
Rene Speck <speckrene@gmail.com>
2022-01-23 18:40:17 +0100

    Updates FeedbackComponent.

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -20,24 +20,54 @@ export class FeedbackComponent {
 //  requiredFileType:string;
 
 //  fileName = '';
-//  uploadProgress:number;
+  uploadProgress:number;
   uploadSub: Subscription;
-
+  httpOptions = {
+    //headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    //responseType: 'json' as const,
+    reportProgress: true,
+    observe: 'events' as const
+  };
   constructor(private http: HttpClient) {}
 
   apiURL: string = 'http://localhost:9081/feedback';
   inputData : any;
+  responseData : any;
 
   submit():void{
     if(this.inputData){
       const file : File = new File([this.inputData], 'inputData.txt', {
         type: 'application/json',
       });
-      console.log('submit feedback');
-      console.log(this.inputData);
-      console.log(file);
+
+      const formData = new FormData();
+      formData.append('feedback', file);
+
+      const upload$ = this.http.post(this.apiURL, formData, this.httpOptions).pipe(
+          finalize(() => this.reset())
+      )
+
+      this.uploadSub = upload$.subscribe(event => {
+        if (event.type == HttpEventType.UploadProgress) {
+          this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+        }
+        if (event.type === HttpEventType.Response) {
+            this.responseData = event.body;
+        }
+      })
+
     }else{
       console.warn('No data given!');
     }
+  }
+
+  cancelUpload() {
+    this.uploadSub.unsubscribe();
+    this.reset();
+  }
+
+  reset() {
+    this.uploadProgress = null;
+    this.uploadSub = null;
   }
 }
