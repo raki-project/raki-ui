a57998221e9a333d99fbf68b2adf34c6f4b17b31
Rene Speck <speckrene@gmail.com>
2022-01-23 19:46:47 +0100

    Updates PrettyJsonPipe.

--- src/app/prettyjson/prettyjson.pipe.ts
+++ src/app/prettyjson/prettyjson.pipe.ts
@@ -5,9 +5,25 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 export class PrettyJsonPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return JSON.stringify(value, null, 4)
-      .replace(/ /g, '&nbsp;') // note the usage of `/ /g` instead of `' '` in order to replace all occurences
-      .replace(/\n/g, '<br/>'); // same here
+  transform(json: any, ...args: any[]): any {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
   }
 }
