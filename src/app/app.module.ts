1ab7f282f6aa131b5c75e8f407842dabffb9d30e
Rene Speck <speckrene@gmail.com>
2022-01-23 18:39:39 +0100

    Imports MatProgressBarModule and NgbModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 // angular
 import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -10,14 +11,13 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatSelectModule} from '@angular/material/select';
+import {MatProgressBarModules} from '@angular/material/progress-bar';
 // app
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 //
 import {InfoDataComponent} from './info-data/info-data.component';
 import {PrettyJsonPipe} from './prettyjson/prettyjson.pipe';
-//
-//
 
 @NgModule({
   declarations: [
@@ -31,7 +31,9 @@ import {PrettyJsonPipe} from './prettyjson/prettyjson.pipe';
     ReactiveFormsModule, FormsModule,
     HttpClientModule,
     //material
-    MatSliderModule, MatIconModule, MatInputModule, MatDividerModule, MatSelectModule
+    MatSliderModule, MatIconModule, MatInputModule, MatDividerModule, MatSelectModule, MatProgressBarModule,
+    //
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
