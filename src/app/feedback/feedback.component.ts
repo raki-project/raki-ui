2ebcfc29874c859ffcb3c225d03adc5122398600
Rene Speck <speckrene@gmail.com>
2022-03-29 17:00:08 +0200

    Updates feedback component.

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -6,70 +6,21 @@ import {FormControl, FormGroup} from '@angular/forms';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatIconModule} from '@angular/material/icon';
 import {ConfigService} from '../app/config.service';
+import { Output, EventEmitter } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 
 @Component({
-  selector: 'feedback',
+  selector: 'feedback-component',
   templateUrl: 'feedback.component.html',
   styleUrls: ['feedback.component.css']
 })
 
 export class FeedbackComponent {
 
-  constructor(
-    private http: HttpClient,
-    private cfg: ConfigService
-  ) {}
+  @Output() feedbackEvent = new EventEmitter<any>();
 
-  uploadProgress: number;
-  uploadSub: Subscription;
-  httpOptions = {
-    //headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-    responseType: 'text' as const,
-    reportProgress: true,
-    observe: 'events' as const
-  };
-
-
-  feedbackAPI: string = this.cfg.api + 'feedback';
-  inputData: any;
-  responseData: any;
-
-  submit(): void {
-    if(this.inputData){
-      const file: File = new File([this.inputData], 'feedback.json', {
-        type: 'application/json',
-      });
-
-      const formData = new FormData();
-      formData.append('feedback', file);
-
-      const upload$ = this.http.post(this.feedbackAPI, formData, this.httpOptions).pipe(
-          finalize(() => this.reset())
-      )
-
-      this.uploadSub = upload$.subscribe(event => {
-        if (event.type == HttpEventType.UploadProgress) {
-          this.uploadProgress = Math.round(100 * (event.loaded / event.total));
-        }
-        if (event.type === HttpEventType.Response) {
-            this.responseData = event.body;
-        }
-      })
-
-    }else{
-      console.warn('No data given!');
-    }
-  }
-
-  cancelUpload() {
-    this.uploadSub.unsubscribe();
-    this.reset();
-  }
-
-  reset() {
-    this.uploadProgress = null;
-    this.uploadSub = null;
+  emitFeedback(value: any) {
+      this.feedbackEvent.emit(value);
   }
 }
