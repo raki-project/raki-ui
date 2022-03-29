7dd8b1fd19d82f1f5bb517e1bc22a1fc68f6ea3f
Rene Speck <speckrene@gmail.com>
2022-03-29 16:57:00 +0200

    Updates cfg.

--- src/app/app/config.service.ts
+++ src/app/app/config.service.ts
@@ -1,5 +1,8 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
 import {Injectable} from '@angular/core';
+import {catchError, map, tap} from 'rxjs/operators';
+import {InfoDataInterface} from './interface/info';
 
 @Injectable({
   providedIn: 'root'
@@ -8,21 +11,51 @@ import {Injectable} from '@angular/core';
 export class ConfigService {
 
   private cfg: any;
+  public info: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient
+  ) {}
 
-  loadConfig() {
-    return this.http.get('/assets/config.json')
-      .toPromise()
-      .then(data => {
-        this.cfg = data;
-      });
+  init() {
+     this.loadConfig().then(()=>{
+        this.loadData().subscribe(data => {
+          this.info = data;
+        })
+    });
   }
 
-  get api() {
+  /** Reads config file */
+  loadConfig() {
+    return this.http.get('/assets/config.json').toPromise().then(
+      data => {this.cfg = data}
+    );
+  }
+
+  get api(): string {
     if (!this.cfg) {
       throw Error('Config file not loaded!');
     }
-    return this.cfg.rakiWebapp;
+    return this.cfg.rakiBackend;
+  }
+
+  get get(): any {
+    if (!this.cfg) {
+      throw Error('Config file not loaded!');
+    }
+    return this.cfg;
+  }
+
+  /** GET data from the server */
+  loadData(): Observable<InfoDataInterface> {
+    return this.http.get<InfoDataInterface>(
+      this.api + 'info',
+      {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+        responseType: 'json' as const
+      })
+      .pipe(
+        //tap(_ => console.log('fetched data'))
+      );
   }
 }
